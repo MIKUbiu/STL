b9b0df436cc79104a90518345f3923f8b8202fc5
MIKUbiu <75475875+MIKUbiu@users.noreply.github.com>
2022-03-13 16:05:06 +0800

    Add files via upload

---
+++ Map_Print_Wordnum.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+#include<vector>
+#include<map>
+using namespace std;
+int main() {
+	map<string, int>word_cnt;
+	string s;
+	while (cin >> s) {
+		word_cnt[s]++;
+	}
+	for (auto e : word_cnt) {
+		cout << e.first << "occurs" << e.second
+			<< (e.second > 1) ? "time" :"times";
+	}
+	return 0;
+}
