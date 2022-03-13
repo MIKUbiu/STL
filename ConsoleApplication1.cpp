70b0a98adbb69113d3d21b16d2889949df671aca
MIKUbiu <75475875+MIKUbiu@users.noreply.github.com>
2022-03-13 15:45:28 +0800

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+﻿#include<iostream>
+#include<vector>
+#include<map>
+using namespace std;
+int main() {
+	map<string, int>S;
+	string s;
+	while (cin >> s) {
+		S[s]++;
+	}
+	cout << S.size();
+	return 0;
+}
