09527535d5593a3908eeb33a4b452c8eb99fe2e3
MIKUbiu <75475875+MIKUbiu@users.noreply.github.com>
2022-03-15 20:25:18 +0800

    Add files via upload

---
+++ Delete_same.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+using namespace std;
+
+int main() {
+	int n; cin >> n;
+	map<int, int>num;
+	int a;
+	while (n--) {
+		cin >> a;
+		++num[a];
+	}
+	for (auto e : num) {
+		cout << e.first << endl;
+	}
+	return 0;
+}
