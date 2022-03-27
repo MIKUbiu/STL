c441e2fe54d2af2b5df76009b13d650274510c86
MIKUbiu <75475875+MIKUbiu@users.noreply.github.com>
2022-03-27 19:55:59 +0800

    Add files via upload

---
+++ map_random.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+#include<map>
+using namespace std;
+template<typename T = int>
+inline T read() {
+	T x;
+	cin >> x;
+	return x;
+}
+int main() {
+	int T;
+	cin >> T;
+	map<int, int>a;
+	while (T--) {
+		int i;
+		cin >> i;
+		a[i]++;
+	}
+	cout << a.size() << endl;
+	for (auto e : a) {
+		cout << e.first << " ";
+	}
+	return 0;
+}
