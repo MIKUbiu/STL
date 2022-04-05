37cb5427ff9d41bffe0f08d24cf09fc41e8565d6
MIKUbiu <75475875+MIKUbiu@users.noreply.github.com>
2022-04-05 16:44:14 +0800

    unordered_set

---
+++ countPrimeSetBits.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+#include<unordered_set>
+using namespace std;
+class Solution {
+public:
+    unordered_set<int> prime = { 2,3,5,7,11,13,17,19,23,29,31 };
+    int countPrimeSetBits(int left, int right) {
+        int ans = 0;
+        for (int i = left; i <= right; i++) {
+            int cnt = __builtin_popcount(i);
+            if (prime.count(cnt)) ans++;//.count(x)查找容器中值为k的个数
+        }
+        return ans;
+    }
+};//这个unordered_set可以存放数据，并用来查找数据个数
+int main() {
+	
+	return 0;
+}
